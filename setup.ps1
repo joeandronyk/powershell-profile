73688a422c6940225dd721aa7fba9c837e124faf
Joe Andronyk <joeandronyk@gmail.com>
2024-06-30 14:33:08 -0700

    Now downloads the theme as well.

--- setup.ps1
+++ setup.ps1
@@ -25,20 +25,15 @@ if (-not (Test-InternetConnection)) {
 if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
     try {
         # Detect Version of PowerShell & Create Profile directories if they do not exist.
-        $profilePath = ""
-        if ($PSVersionTable.PSEdition -eq "Core") { 
-            $profilePath = "$env:userprofile\Documents\Powershell"
-        }
-        elseif ($PSVersionTable.PSEdition -eq "Desktop") {
-            $profilePath = "$env:userprofile\Documents\WindowsPowerShell"
-        }
+        $profilePath = Split-Path -parent $PROFILE
 
         if (!(Test-Path -Path $profilePath)) {
             New-Item -Path $profilePath -ItemType "directory"
         }
 
         Invoke-RestMethod https://github.com/joeandronyk/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $PROFILE
-        Write-Host "The profile @ [$PROFILE] has been created."
+        Invoke-RestMethod https://github.com/joeandronyk/powershell-profile/raw/main/powershell-theme.omp.json -OutFile $profilePath\powershell-theme.omp.json
+        Write-Host "The profile @ [$PROFILE] has been created and the oh-my-posh theme powershell-theme.omp.json has been added."
         Write-Host "If you want to make any personal changes or customizations, please do so at [$profilePath\Profile.ps1] as there is an updater in the installed profile which uses the hash to update the profile and will lead to loss of changes"
     }
     catch {
@@ -49,6 +44,7 @@ else {
     try {
         Get-Item -Path $PROFILE | Move-Item -Destination "oldprofile.ps1" -Force
         Invoke-RestMethod https://github.com/joeandronyk/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $PROFILE
+        Invoke-RestMethod https://github.com/joeandronyk/powershell-profile/raw/main/powershell-theme.omp.json -OutFile $profilePath\powershell-theme.omp.json
         Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
         Write-Host "Please back up any persistent components of your old profile to [$HOME\Documents\PowerShell\Profile.ps1] as there is an updater in the installed profile which uses the hash to update the profile and will lead to loss of changes"
     }
