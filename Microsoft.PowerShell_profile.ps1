0e624412c5dc34515ef792eb9621a32808cf9de7
Joe Andronyk <joeandronyk@gmail.com>
2024-08-13 09:35:25 -0700

    Trying again

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -80,9 +80,9 @@ Update-PowerShell
 $isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 function prompt {
     if ($isAdmin) { "[" + (Get-Location) + "] # " } else { "[" + (Get-Location) + "] $ " }
+    $Host.UI.RawUI.WindowTitle = Split-Path -Leaf (Get-Location)
 }
 $adminSuffix = if ($isAdmin) { " [ADMIN]" } else { "" }
-$Host.UI.RawUI.WindowTitle = Split-Path -Leaf (Get-Location)
 
 # Utility Functions
 function Test-CommandExists {
