9cc5c758e034441d2f9b81c907a9bd1fafd7783b
Joe Andronyk <joeandronyk@gmail.com>
2024-08-13 09:23:08 -0700

    Changing the name of the tab based on cwd

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -82,7 +82,7 @@ function prompt {
     if ($isAdmin) { "[" + (Get-Location) + "] # " } else { "[" + (Get-Location) + "] $ " }
 }
 $adminSuffix = if ($isAdmin) { " [ADMIN]" } else { "" }
-$Host.UI.RawUI.WindowTitle = "PowerShell {0}$adminSuffix" -f $PSVersionTable.PSVersion.ToString()
+$Host.UI.RawUI.WindowTitle = Split-Path -Leaf (Get-Location)
 
 # Utility Functions
 function Test-CommandExists {
